e4d5620a25b766a39e9686ee4484c96204cf343a
Yunsheng Tian <yunsheng@mit.edu>
2020-11-19 23:48:13 -0500

    bug fix

--- system/scientist/gui/panel_control/stop_criterion/view.py
+++ system/scientist/gui/panel_control/stop_criterion/view.py
@@ -7,7 +7,7 @@ class StopCriterionView:
     def __init__(self, root_view):
         self.root_view = root_view
 
-        self.window = tk.Toplevel(master=self.root_view.root)
+        self.window = tk.Toplevel(master=self.root_view.root_view.root)
         self.window.title('Stopping Criterion')
         self.window.resizable(False, False)
 
