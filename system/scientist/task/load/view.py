bef8181dfd5c5de585ca7ff3b289b41f71d3d4d4
Yunsheng Tian <yunsheng@mit.edu>
2021-01-16 15:09:03 -0500

    bug fix due to the rename

--- system/scientist/task/load/view.py
+++ system/scientist/task/load/view.py
@@ -9,7 +9,7 @@ class LoadTaskView:
         self.window.title('Load Task')
         self.window.resizable(False, False)
         self.widget = {}
-        self.widget['table_name'] = create_widget('labeled_combobox', master=self.window, row=0, column=0, columnspan=2, 
-            text='Table name', required=True)
+        self.widget['task_name'] = create_widget('labeled_combobox', master=self.window, row=0, column=0, columnspan=2, 
+            text='Task name', required=True)
         self.widget['load'] = create_widget('button', master=self.window, row=1, column=0, text='Load')
         self.widget['cancel'] = create_widget('button', master=self.window, row=1, column=1, text='Cancel')
--- system/scientist/task/remove/view.py
+++ system/scientist/task/remove/view.py
@@ -9,7 +9,7 @@ class RemoveTaskView:
         self.window.title('Remove Task')
         self.window.resizable(False, False)
         self.widget = {}
-        self.widget['table_name'] = create_widget('labeled_combobox', master=self.window, row=0, column=0, columnspan=2, 
-            text='Table name', required=True)
+        self.widget['task_name'] = create_widget('labeled_combobox', master=self.window, row=0, column=0, columnspan=2, 
+            text='Task name', required=True)
         self.widget['remove'] = create_widget('button', master=self.window, row=1, column=0, text='Remove')
         self.widget['cancel'] = create_widget('button', master=self.window, row=1, column=1, text='Cancel')
