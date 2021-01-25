da89e6fd589374a612f7e9a2ca62ceea0b08751e
Yunsheng Tian <yunsheng@mit.edu>
2021-01-24 20:53:44 -0500

    suppress warnings

--- scripts/team/run_scientist.py
+++ scripts/team/run_scientist.py
@@ -2,6 +2,9 @@ import os, sys
 root_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..')
 sys.path.append(root_dir)
 
+import warnings
+warnings.filterwarnings('ignore')
+
 from system.scientist.controller.team import ScientistController
 
 
