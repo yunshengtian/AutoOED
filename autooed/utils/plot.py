073e684650f5a8225aa93e62f848a23f57e15d2c
Yunsheng Tian <yunsheng@mit.edu>
2021-11-15 00:47:33 -0500

    Update high-dim plot for cmd

--- autooed/utils/plot.py
+++ autooed/utils/plot.py
@@ -1,7 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
 from mpl_toolkits.mplot3d import Axes3D
 from pymoo.factory import get_performance_indicator
+
+
+def parallel_transform(Y):
+    '''
+    Transform performance values from cartesian to parallel coordinates
+    '''
+    Y = np.array(Y)
+    return np.dstack([np.vstack([np.arange(Y.shape[1])] * len(Y)), Y])
 
 
 def plot_performance_space(Y):
@@ -17,6 +26,14 @@ def plot_performance_space(Y):
         fig = plt.figure()
         ax = fig.gca(projection='3d')
         ax.scatter(*Y.T)
+        plt.show()
+    elif Y.shape[1] > 3:
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        segments = parallel_transform(Y)
+        ax.add_collection(LineCollection(segments))
+        ax.set_xlim(0, Y.shape[1] - 1)
+        ax.set_ylim(np.min(Y), np.max(Y))
         plt.show()
     else:
         raise Exception(f'Objectives with dimension {Y.shape[1]} is not supported')
